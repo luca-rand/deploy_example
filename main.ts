c5b94a2ac9952f2052b99c3ee883cf766d25b41d
Luca Casonato <lucacasonato@yahoo.com>
2020-12-21 22:24:17 +0100

    x

---
+++ main.ts
@@ -0,0 +1,12 @@
+addEventListener("fetch", (event) => {
+  event.respondWith(handle(event.request));
+});
+
+async function handle(request: Request): Promise<Response> {
+  return new Response("Hello world", {
+    status: 200,
+    headers: {
+      "content-type": "text/plain",
+    },
+  });
+}
